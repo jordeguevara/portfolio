9122f031b8cb7e1a14961708564f0f8d95f97233
Jorde G <34716202+jordeguevara@users.noreply.github.com>
2018-02-06 22:40:57 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+router.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+router.get('/blog', function(req, res) {
+  res.render('pages/blog');
+});
+
+router.get('/contact', function(req, res) {
+  res.render('pages/contact');
+});
+
+router.get('/post', function(req, res) {
+  res.render('pages/post');
+});
+
+module.exports = router;
