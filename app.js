66e727d926e752f3b2d59a4afc4602fe690e2a90
Jorde Guevara <jguevara60@toromail.csudh.edu>
2019-08-06 22:05:43 -0700

    refactored

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
 
-var app = express();
-//tes
+const app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -14,9 +13,8 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const rp = require('request-promise');
+const {GITHUB_TOKEN} = require('../config')
 
-/* GET home page. */
-// router.get('/', function(req, res) {
-//   res.render('pages/index');
-// });
-
-/* GET home page. */
 router.get('/', function(req, res) {
 	res.render('pages/about');
-});
-
-router.get('/about', function(req, res) {
-  res.render('pages/index');
-});
-
-router.get('/blog', function(req, res) {
-  res.render('pages/blog');
-});
-
-router.get('/contact', function(req, res) {
-  res.render('pages/contact');
-});
-
-router.get('/post', function(req, res) {
-  res.render('pages/post');
 });
 
 module.exports = router;
