32c5cf177cf2de55b89f0d917f2d76bdcb636a80
Jorde Guevara <jguevara60@toromail.csudh.edu>
2019-11-06 12:55:06 -0800

    added meta data and removed menu

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,17 @@ router.get('/', function(req, res) {
 	res.render('pages/about');
 });
 
+router.get('/tut', function(req, res) {
+    var tuts = [
+		{ title: 'Intro to Three.js (coming soon)',
+		  description: 'OpenGL for the Web! How to use WebGL via Three.js', tag:'Javascript'},
+    ];
+	res.render('pages/tut',{
+        tuts:tuts
+	});
+});
+
+
 router.get('/download',function(req,res){
 	res.download('/Users/jordeguevara/Desktop/projects/portfolio/jorde_6.pdf');
 })
